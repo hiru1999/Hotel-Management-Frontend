ae9148e9ead87ebdd019b28ed5358189ad97bddc
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-23 00:22:44 +0530

    Remove routes in AdminPage

--- src/pages/admin-page/adminPage.jsx
+++ src/pages/admin-page/adminPage.jsx
@@ -2,46 +2,8 @@ import { Route, Routes } from "react-router-dom";
 
 export default function AdminPage(){
     return(
-        <div className="w-full h-[100vh] bg-red-400">
+        <div className="w-full h-[100vh]">
             
-            <Routes path="/*">
-                <Route path="/bookings" element={
-                    <div>
-                        <h1>Bookings</h1>
-                    </div>
-                } />
-            
-                <Route path="/categories" element={
-                    <div>
-                        <h1>Categories</h1>
-                    </div>
-                } />
-            
-                <Route path="/rooms" element={
-                    <div>
-                        <h1>Rooms</h1>
-                    </div>
-                } />
-            
-                <Route path="/users" element={
-                    <div>
-                        <h1>Users</h1>
-                    </div>
-                } />
-            
-                <Route path="/feedbacks" element={
-                    <div>
-                        <h1>Feedbacks</h1>
-                    </div>
-                } />
-            
-                <Route path="/galleryitems" element={
-                    <div>
-                        <h1>Gallery Items</h1>
-                    </div>
-                } />
-
-            </Routes>
             
         </div>
     )
