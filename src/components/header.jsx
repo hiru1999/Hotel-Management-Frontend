1f1e5497e8ff32309c7cf782924c04748ff4fd81
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-22 21:02:17 +0530

    Text tailwind.css working correctly

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import "./header.css";
 function Header(){
     return(
         <header>
-            <h1>Hotel management system</h1>
+            <h1 className="text-yellow-400">Hotel management system</h1>
             <UserTag imageLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnS1o3mO3S_Nkfw1WAGaRJ6KaOGgODpfoOsA&s" name="Hiruvi Senadheera"/>
         </header>
     )
