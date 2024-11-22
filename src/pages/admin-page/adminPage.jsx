46b75e103926c470989900b3c02ee1a81e1f278c
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-23 01:43:02 +0530

    Create links to navigate pages from adminPage

--- src/pages/admin-page/adminPage.jsx
+++ src/pages/admin-page/adminPage.jsx
@@ -1,10 +1,32 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 
 export default function AdminPage(){
     return(
-        <div className="w-full max-h-[100vh] overflow-hidden overflow-y-hidden">
+        <div className="w-full max-h-[100vh] overflow-hidden flex">
             
-
+            <div className="w-[20%] bg-blue-400 h-[100vh] flex flex-col">
+                <div>
+                    <Link to="/admin/bookings">Bookings</Link>
+                </div>
+                <div>
+                    <Link to="/admin/categories">Categories</Link>
+                </div>
+                <div>
+                    <Link to="/admin/rooms">Rooms</Link>
+                </div>
+                <div>
+                    <Link to="/admin/users">Users</Link>
+                </div>
+                <div>
+                    <Link to="/admin/feedbacks">Feedbacks</Link>
+                </div>
+                <div>
+                    <Link to="/admin/galleryitems">Gallery Items</Link>
+                </div>
+            </div>
+            <div className="w-[80%] bg-blue-900">
+                
+            </div>
             
         </div>
     )
