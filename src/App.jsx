9453a741979cc11925f1a86cf0083ae8053cb1a6
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-22 22:46:07 +0530

    Create div and add styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ function App() {
       <Header/>
       <div className="w-full h-screen bg-blue-900 flex">
 
+        <div className="border border-white bg-white h-[100px] w-[700px] rounded-lg">
+
+        </div>
+
       </div>
     </>
   )
