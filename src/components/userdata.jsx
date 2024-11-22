733324e81d1f6dcbb3818e0191b2c5a126fd8b53
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-22 15:41:04 +0530

    Add styles to image

--- src/components/userdata.jsx
+++ src/components/userdata.jsx
@@ -1,7 +1,7 @@
 function UserTag(){
     return(
         <div>
-            <img src="https://cdn-icons-png.flaticon.com/512/219/219970.png" />
+            <img src="https://cdn-icons-png.flaticon.com/512/219/219970.png" style={{width : 50}} />
             <h1>John Doe</h1>
         </div>
     )
