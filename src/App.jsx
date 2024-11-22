bc3d1b3ca015bbd0de6246f2738e49f9314eb05c
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-22 23:42:08 +0530

    Create routers for homePage and adminPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import Header from "./components/header"
 import './App.css';
 import HomePage from "./pages/client-page/homePage";
 import AdminPage from "./pages/admin-page/adminPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   
 
   return (
     <>
-      <HomePage/>
-  
+      <BrowserRouter>
+        <Routes path="/*">
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/admin" element={<AdminPage/>}/>
+          <Route path="/*" element={
+            <div className="w-full h-[100vh] bg-green-400">
+              404 NOT FOUND
+            </div>
+          }/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
