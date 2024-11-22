3a48d1bde6b6b3c81d0f15ae1f0085b215500635
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-23 00:05:14 +0530

    Create routers from admin page

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes path="/*">
           <Route path="/" element={<HomePage/>}/>
-          <Route path="/admin" element={<AdminPage/>}/>
+          <Route path="/admin/*" element={<AdminPage/>}/>
           <Route path="/*" element={
             <div className="w-full h-[100vh] bg-green-400">
               404 NOT FOUND
--- src/pages/admin-page/adminPage.jsx
+++ src/pages/admin-page/adminPage.jsx
@@ -1,7 +1,53 @@
+import { Route, Routes } from "react-router-dom";
+
 export default function AdminPage(){
     return(
         <div className="w-full h-[100vh] bg-red-400">
-            Admin
+            
+            <Routes path="/*">
+                <Route path="/bookings" element={
+                    <div>
+                        <h1>Bookings</h1>
+                    </div>
+                } />
+            
+                <Route path="/categories" element={
+                    <div>
+                        <h1>Categories</h1>
+                    </div>
+                } />
+            
+                <Route path="/rooms" element={
+                    <div>
+                        <h1>Rooms</h1>
+                    </div>
+                } />
+            
+                <Route path="/users" element={
+                    <div>
+                        <h1>Users</h1>
+                    </div>
+                } />
+            
+                <Route path="/feedbacks" element={
+                    <div>
+                        <h1>Feedbacks</h1>
+                    </div>
+                } />
+            
+                <Route path="/galleryitems" element={
+                    <div>
+                        <h1>Gallery Items</h1>
+                    </div>
+                } />
+
+                {/* <Route path="/*" element={
+                    <div className="w-full h-[100vh] bg-green-400">
+                    404 NOT FOUND
+                    </div>
+                }/> */}
+            </Routes>
+            
         </div>
     )
 }
