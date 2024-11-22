f02233877dcd23b1c2b682c4301aa25b6dd9b64f
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-23 01:30:06 +0530

    Create div and add styles in adminPage

--- src/pages/admin-page/adminPage.jsx
+++ src/pages/admin-page/adminPage.jsx
@@ -2,8 +2,9 @@ import { Route, Routes } from "react-router-dom";
 
 export default function AdminPage(){
     return(
-        <div className="w-full h-[100vh]">
+        <div className="w-full max-h-[100vh] overflow-hidden overflow-y-hidden">
             
+
             
         </div>
     )
