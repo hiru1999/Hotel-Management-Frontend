fe15c3bcd9f357d18fbfb2b776535adf830ed78d
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-23 13:15:42 +0530

    Create TestComponent and navigate it

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from "./pages/client-page/homePage";
 import AdminPage from "./pages/admin-page/adminPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TestComponent from "./components/test/test";
 
 function App() {
   
@@ -12,7 +13,8 @@ function App() {
       <BrowserRouter>
         <Routes path="/*">
           <Route path="/admin/*" element={<AdminPage/>}/>
-          <Route path="/" element={<HomePage/>}/>
+          <Route path="/test" element={<TestComponent/>}/>
+          <Route path="/*" element={<HomePage/>}/>
           
         </Routes>
       </BrowserRouter>
---
+++ src/components/test/test.jsx
@@ -0,0 +1,9 @@
+export default function TestComponent(){
+    return(
+
+        <div className="bg-red-900 w-[60px] h-[60px]">
+            test
+        </div>
+
+    )
+}
