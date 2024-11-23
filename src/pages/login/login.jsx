0b0f2c8b662e08e85fa71ba35cd7a815287ab414
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-23 15:34:00 +0530

    Create heading and add styles for login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -4,8 +4,8 @@ export default function LoginPage(){
     return(
 
         <div className="w-full h-[100vh] pic-bg flex justify-center items-center">
-                <div className="w-[400px] h-[400px] backdrop-blur-md rounded-lg">
-
+                <div className="w-[400px] h-[400px] backdrop-blur-md rounded-lg flex flex-col">
+                    <h1 className="text-3xl p-[15px] text-white text-center">Login</h1>
                 </div>
         </div>
 
