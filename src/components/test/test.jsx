f1106023991ace5e2b36c7b3917a209dcc91cd56
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-23 14:12:15 +0530

    Use useState and create setNum functions

--- src/components/test/test.jsx
+++ src/components/test/test.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react"
+
 export default function TestComponent(){
 
-    let num = 0;
+    const [num,setNum] = useState(0)
 
     return(
 
         <div className="bg-blue-950 w-full h-[100vh] flex justify-center items-center">
             <div className="bg-white w-[350px] h-[350px] flex justify-center items-center">
-                <button className="w-[60px] h-[60px] bg-blue-500 rounded-full text-2xl text-white text-center">
+                <button className="w-[60px] h-[60px] bg-blue-500 rounded-full text-2xl text-white text-center"
+                onClick={
+                    ()=>{
+                        setNum(num-1)
+                        
+                    }
+                }>
                     -
                 </button>
                 <span className="text-6xl ml-5 mr-5">
@@ -15,7 +23,8 @@ export default function TestComponent(){
                 <button className="w-[60px] h-[60px] bg-blue-500 rounded-full text-2xl text-white text-center"
                 onClick={
                     ()=>{
-                        alert("Plus button clicked")
+                        setNum(num+1)
+                        
                     }
                 }>
                     +
