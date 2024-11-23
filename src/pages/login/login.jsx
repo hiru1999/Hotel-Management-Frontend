d03dab775a73abdf67828dfd39d5b79f20c3c777
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-23 15:31:37 +0530

    Create div for login and add styles

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -3,8 +3,10 @@ import "./login.css"
 export default function LoginPage(){
     return(
 
-        <div className="w-full h-[100vh] pic-bg">
+        <div className="w-full h-[100vh] pic-bg flex justify-center items-center">
+                <div className="w-[400px] h-[400px] backdrop-blur-md rounded-lg">
 
+                </div>
         </div>
 
     )
