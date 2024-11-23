46d73df80ce28058e699cc4553ae1e0c42078eb2
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-23 15:55:38 +0530

    Create button and change styles

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -4,11 +4,13 @@ export default function LoginPage(){
     return(
 
         <div className="w-full h-[100vh] pic-bg flex justify-center items-center">
-                <div className="w-[400px] h-[400px] backdrop-blur-md rounded-lg flex flex-col items-center">
-                    <h1 className="text-3xl p-[15px] text-white text-center">Login</h1>
-                    <input type="text" placeholder="Enter your email" className="w-[80%] h-[40px] bg-[#00000000] border-[2px] text-white placeholder:text-white px-[5px] rounded-md mb-3"/>
-                    <input type="password" placeholder="Enter your password" className="w-[80%] h-[40px] bg-[#00000000] border-[2px] text-white placeholder:text-white px-[5px] rounded-md mb-3"/>
-
+                <div className="w-[400px] h-[400px] backdrop-blur-md rounded-lg flex flex-col items-center justify-center relative">
+                    <h1 className="text-3xl p-[15px] text-white text-center absolute top-[40px]">Login</h1>
+                    <input type="text" placeholder="Enter your email" className="w-[80%] h-[40px] bg-[#00000000] border-[2px] text-white placeholder:text-white px-[5px] rounded-md mb-[20px]"/>
+                    <input type="password" placeholder="Enter your password" className="w-[80%] h-[40px] bg-[#00000000] border-[2px] text-white placeholder:text-white px-[5px] rounded-md"/>
+                    <button className="w-[80%] bg-red-500 text-white h-[50px] rounded-md absolute bottom-[40px]">
+                        Login
+                    </button>
                 </div>
         </div>
 
