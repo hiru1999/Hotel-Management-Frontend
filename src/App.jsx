0d7ab8bfce5f31c0a53d6325ed8abc261f6055d3
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-27 20:14:04 +0530

    Create categories page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/client-page/homePage";
 import AdminPage from "./pages/admin-page/adminPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login/login";
+import CategoriesPage from "./pages/client-page/categories";
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
         <Routes path="/*">
           <Route path="/admin/*" element={<AdminPage/>}/>
           <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/categories" element={<CategoriesPage/>}/>
           <Route path="/*" element={<HomePage/>}/>
           
         </Routes>
