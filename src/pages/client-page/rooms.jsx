7c29421b6a3ec725afd1d0204e3d0d02509121d5
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-28 13:36:08 +0530

    Call deleteItem function

--- src/pages/client-page/rooms.jsx
+++ src/pages/client-page/rooms.jsx
@@ -71,7 +71,7 @@ export default function RoomsPage(){
                 <td className="border border-gray-300 px-4 py-2">{room.notes || "N/A"}</td>
 
                 <td className="border border-gray-300 px-4 py-2">
-                    <button className="bg-red-500 text-white px-2 py-1 rounded-lg ml-2">
+                    <button onClick={()=>{deleteItem(room.roomId)}} className="bg-red-500 text-white px-2 py-1 rounded-lg ml-2">
                         Delete
                     </button>
                 </td>
